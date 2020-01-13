daf0d50387d4758a258165198c4ac0b75589aca6
Ozair Farahi <ozairfarahi6@gmail.com>
2020-01-13 19:39:43 +0430

    Route implemented

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage";
 
-const HatPage = () => (
-    <div>
-        <h1>Hats</h1>
-    </div>
-);
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
-                <Route path='/hats' component={HatPage}/>
             </Switch>
         </div>
     );
