996da57a6bdc89a1c13f689af6b73873d62c9c03
Ozair Farahi <ozairfarahi6@gmail.com>
2020-02-01 14:26:27 +0430

    Sign In(With email and password) Added

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.scss";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth,signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,14 +14,21 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email,password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email,password);
+
+      this.setState({ email: "", password: "" });
+    }catch (error) {
+      
+    }
+
   };
 
   handleChange = event => {
     const { value, name } = event.target;
-
     this.setState({ [name]: value });
   };
 
