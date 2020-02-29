6f2cdf6f312f73b899011d93a901f47384635b70
Ozair Farahi <ozairfarahi6@gmail.com>
2020-02-29 20:41:09 +0430

    DEV and PRO In Redux Store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,24 @@
-import { createStore, applyMiddleware, compose } from "redux";
-//import { createStore, applyMiddleware } from "redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 // For DEV
-export const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-//const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// export const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(...middlewares),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
